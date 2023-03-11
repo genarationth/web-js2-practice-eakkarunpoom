fb2d7befcb45c08d2dda7e8d1a4a1b750f09a8ac
eakkarunmoss <85754575+eakkarunmoss@users.noreply.github.com>
2023-03-11 16:13:14 +0700

    add JS homework complete task

---
+++ app.js
@@ -0,0 +1,15 @@
+// Challenge 1
+
+let birthDay = 1999;
+let onFuture = 2099;
+
+console.log(`In the year 2099 I will be ${onFuture - birthDay} years old.`);
+
+// Challenge 2
+
+const person = {
+  name : "Moss",
+  place : "Thailand",
+  hobby : "Watch TV"
+};
+console.log("Hi, I am" + " " + person.name + " " + "i live in" + " " + person.place + " " + "and i like" + " " + person.hobby )
